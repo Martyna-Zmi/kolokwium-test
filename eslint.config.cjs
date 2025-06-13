0cac4068e857a61381be72502cb29509885f798c
Martyna-Zmi <s26977@pjwstk.edu.pl>
2025-06-13 09:23:03 +0200

    added lint

---
+++ eslint.config.cjs
@@ -0,0 +1,25 @@
+const js = require('@eslint/js');
+const globals = require('globals');
+
+module.exports = [
+    {
+        ignores: ['node_modules/**', 'dist/**'],
+    },
+    {
+        files: ['**/*.js'],
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: {
+                ...globals.node,
+                ...globals.browser,
+                ...globals.jest,
+            },
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            'no-unused-vars': 'warn',
+            'no-console': 'off',
+        },
+    },
+];
