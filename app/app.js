21a23465b8e6e7e8baee5c1b1fb3d84c01fc35ec
Martyna-Zmi <s26977@pjwstk.edu.pl>
2025-06-13 10:58:35 +0200

    code cleanup commit

--- app/app.js
+++ app/app.js
@@ -8,10 +8,12 @@ app.get('/status', (req, res) => {
 app.get('/data', (req, res) => {
     res.status(200).json({ message: "Hello world!" });
 });
+
 app.get("/joke", async (req, res) => {
     await fetch("https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=single")
         .then(data => data.json())
         .then(dataJson => res.status(200).json(dataJson))
         .catch(() => res.status(500).json({message: "Trouble connecting to the external api"}))
 });
+
 module.exports = app;
